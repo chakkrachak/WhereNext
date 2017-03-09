9697704c1b3638f07baf9f3899c1fce6906aab00
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-03-09 11:34:57 +0100

    Added Podfile

---
+++ PickItWhenIt/PickItWhenIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PickItWhenIt
+//
+//  Created by Chakkra CHAK on 09/03/2017.
+//  Copyright © 2017 Chakkra CHAK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
