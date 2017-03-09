f3d8a9b0be0c911d7c81012a2ea75291a3f174d7
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-03-09 12:11:24 +0100

    Add table view

--- PickItWhenIt/PickItWhenIt/ViewController.swift
+++ PickItWhenIt/PickItWhenIt/ViewController.swift
@@ -8,7 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StopPointViewCell: UITableViewCell {
+    @IBOutlet weak var label: UILabel!
+    
+    func updateWith(label:String) {
+        self.label.text = label
+    }
+}
+
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let token:String = "9e304161-bb97-4210-b13d-c71eaf58961c"
+    let coverage:String = "fr-idf"
+    
+    let cellIdentifier = "StopPointCellIdentifier"
+    
+    var stopSchedules:[String] = ["TATA", "TOTO", "TUTU"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +35,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stopSchedules.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath as IndexPath) as! StopPointViewCell
+        
+        cell.updateWith(label:stopSchedules[indexPath.row])
+        
+        return cell
+    }
 }
 
